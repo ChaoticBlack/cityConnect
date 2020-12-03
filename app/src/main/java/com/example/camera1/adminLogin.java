e363154e1ea0e188005c1445fcdd9510696bc2a9
ChaoticBlack <shivamraje43@gmail.com>
2020-12-03 10:05:12 +0530

    finally done?

--- app/src/main/java/com/example/camera1/adminLogin.java
+++ app/src/main/java/com/example/camera1/adminLogin.java
@@ -1,14 +1,18 @@
 package com.example.camera1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class adminLogin extends AppCompatActivity {
     static EditText Username;
@@ -21,10 +25,38 @@ public class adminLogin extends AppCompatActivity {
     String userPassword = "";
     boolean crosscheck = false;
     private int counter = 5;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent in;
+            switch (item.getItemId()) {
+                case R.id.navigation_home: {
+                    in=new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(in);
+                    return true;
+                }
+                case R.id.map_dashboard: {
+
+                    in=new Intent(getBaseContext(),maps.class);
+                    startActivity(in);
+                    return true;
+                }
+                case R.id.rate_us:
+                    Toast.makeText(getApplicationContext(),"Review",Toast.LENGTH_LONG).show();
+                    return true;
+            }
+            return false;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Username = findViewById(R.id.username);
         Password = findViewById(R.id.password);
         Attempts = findViewById(R.id.attemptinfo);
